1509e6acad9e91a15dd173c73cbb1b1b19e3a41a
lgurgand <82041079+lgurgand@users.noreply.github.com>
2021-04-29 10:13:02 +0200

    Add files via upload

---
+++ projet pcbs avec image.py
@@ -0,0 +1,71 @@
+
+"""Flanker task
+
+At each trial, a set of arrows is presented at the center of the
+screen and the participant must look at the middle arrow, then press the left arrow key if the middle arrow points the left, and the right arrow key if the middle arrow points the right.
+
+
+"""
+
+
+import random
+from expyriment import design, control, stimuli
+import expyriment
+
+MAX_RESPONSE_DELAY = 2000
+LEFT_RESPONSE = 'Left arrow'
+RIGHT_RESPONSE = 'Right arrow'
+REPETITIONS=8
+TRIALS = REPETITIONS*4
+
+exp = design.Experiment(name="Flanker Task", text_size=40)
+expyriment.io.defaults.outputfile_time_stamp = False
+
+control.initialize(exp)
+
+pictures = {"lcong": expyriment.stimuli.Picture("C:/Users/Utilisateur/Desktop/lilas/left_congruent.png"),"rcong": expyriment.stimuli.Picture("C:/Users/Utilisateur/Desktop/lilas/right_congruent.png"),"lincong": expyriment.stimuli.Picture("C:/Users/Utilisateur/Desktop/lilas/left_incongruent.png"),"rincong": expyriment.stimuli.Picture("C:/Users/Utilisateur/Desktop/lilas/right_incongruent.png")}
+
+
+cue = stimuli.FixCross(size=(50, 50), line_width=4)
+blankscreen = stimuli.BlankScreen()
+instructions = stimuli.TextScreen("Instructions",
+    f"""When you'll see a set of arrows, your task to decide, as quickly as possible, whether the middle arrow points the left or the right.
+
+    if it points the left, press '{LEFT_RESPONSE}'
+
+    if it points the right, press '{RIGHT_RESPONSE}'
+
+    There will be '{TRIALS}' trials in total.
+
+    Press the space bar to start.""")
+
+
+exp.add_data_variable_names(['stimulus', 'respkey', 'RT'])
+
+control.start(skip_ready_screen=True)
+instructions.present()
+exp.keyboard.wait()
+
+for i in range (0,REPETITIONS):
+    #randomize order of stimulus presentation
+    #within each repetition of the 4 pictures
+    random_pictures={}
+    items = list(pictures.items())  # List of tuples of (key,values)
+    random.shuffle(items)
+    for key, value in items:
+        random_pictures[key]=value
+
+    pictures = random_pictures
+
+    for picture in pictures:
+        pictures[picture].preload()
+        blankscreen.present()
+        exp.clock.wait(1000)
+        cue.present()
+        exp.clock.wait(500)
+        pictures[picture].present()
+        key, rt = exp.keyboard.wait( duration=MAX_RESPONSE_DELAY)
+
+        exp.data.add([picture,  key, rt])
+
+control.end()
